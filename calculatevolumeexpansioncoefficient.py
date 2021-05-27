6fa245a22fffa19e33b39f5e05ae158de6c4cd39
albertymh <74096783+albertymh@users.noreply.github.com>
2021-05-27 14:46:21 +0800

    Add files via upload

---
+++ calculatevolumeexpansioncoefficient.py
@@ -0,0 +1,8 @@
+import math
+m1 = float(input('m1 = '))
+m2 = float(input('m2 = '))
+t1 = float(input('t1 = '))
+t2 = float(input('t2 = '))
+m = 154.65
+alpha = float((m2 - m1)/(m - m2)/(t2 - t1))
+print(alpha)
